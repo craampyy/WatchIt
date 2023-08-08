5cd884d60f3a7ba675dce429d88640080157a7e9
craampyy <craampyy@gmail.com>
2023-08-08 17:02:48 +0200

    0.0.4 - View All Popular

---
+++ js/popular-movies.js
@@ -0,0 +1,94 @@
+const apiKey = config.apiKey; // Reemplaza con tu clave de API
+const moviesListSection = document.querySelector(".list");
+
+const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US`;
+
+fetch(popularMoviesUrl)
+  .then((response) => response.json())
+  .then((data) => {
+    const popularMovies = data.results; // Obtener todas las películas populares de la primera página
+    getGenresAndDisplay(popularMovies);
+  })
+  .catch((error) => console.error("Error fetching popular movies:", error));
+
+function getGenresAndDisplay(movies) {
+  const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`;
+
+  fetch(genreUrl)
+    .then((response) => response.json())
+    .then((genreData) => {
+      const genreMap = {};
+      genreData.genres.forEach((genre) => {
+        genreMap[genre.id] = genre.name;
+      });
+
+      movies.forEach((movie) => {
+        const genreNames = movie.genre_ids
+          .slice(0, 2)
+          .map((id) => genreMap[id]);
+
+        movie.genres = genreNames;
+      });
+
+      displayMovies(movies);
+    })
+    .catch((error) => console.error("Error fetching genres:", error));
+}
+
+function displayMovies(movies) {
+  movies.forEach((movie) => {
+    const article = document.createElement("article");
+    article.classList.add("card");
+
+    const link = document.createElement("a");
+    link.href = "#";
+
+    const img = document.createElement("img");
+    img.src = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
+    img.alt = `Poster of ${movie.title}`;
+    img.width = 200;
+
+    const info = document.createElement("div");
+    info.classList.add("info");
+
+    const title = document.createElement("span");
+    title.id = "title";
+    title.textContent = movie.title;
+
+    const ratingDiv = document.createElement("div");
+    ratingDiv.classList.add("rating");
+
+    const starIcon = document.createElement("span");
+    starIcon.classList.add("material-icons-sharp");
+    starIcon.textContent = "star";
+
+    const ratingNumber = document.createElement("span");
+    ratingNumber.id = "number";
+    ratingNumber.textContent = movie.vote_average;
+
+    ratingDiv.appendChild(starIcon);
+    ratingDiv.appendChild(ratingNumber);
+
+    info.appendChild(title);
+    info.appendChild(ratingDiv);
+
+    const categoriesDiv = document.createElement("div");
+    categoriesDiv.classList.add("categories");
+
+    movie.genres.forEach((genreName) => {
+      const genreSpan = document.createElement("span");
+      genreSpan.textContent = genreName;
+      categoriesDiv.appendChild(genreSpan);
+    });
+
+    link.appendChild(img);
+    link.appendChild(info);
+    article.appendChild(link);
+    article.appendChild(categoriesDiv);
+
+    moviesListSection.appendChild(article);
+
+    console.log(movie); // Mostrar información en la consola
+  });
+}
+
--- js/popular-series.js
+++ js/popular-series.js
@@ -1,32 +1,18 @@
 const apiKey = config.apiKey; // Reemplaza con tu clave de API
-const moviesListSection = document.querySelector(
-  ".most-popular.movies .resumed-list"
-);
-const seriesListSection = document.querySelector(
-  ".most-popular.series .resumed-list"
-);
+const seriesListSection = document.querySelector(".list");
 
-const popularMoviesUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
-const popularSeriesUrl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US&page=1`;
-
-fetch(popularMoviesUrl)
-  .then((response) => response.json())
-  .then((data) => {
-    const popularMovies = data.results.slice(0, 7); // Obtener las primeras 7 películas
-    getGenresAndDisplay(popularMovies, moviesListSection);
-  })
-  .catch((error) => console.error("Error fetching popular movies:", error));
+const popularSeriesUrl = `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}&language=en-US`;
 
 fetch(popularSeriesUrl)
   .then((response) => response.json())
   .then((data) => {
-    const popularSeries = data.results.slice(0, 7); // Obtener las primeras 7 series populares
-    getGenresAndDisplay(popularSeries, seriesListSection);
+    const popularSeries = data.results; // Obtener todas las series populares de la primera página
+    getGenresAndDisplay(popularSeries);
   })
   .catch((error) => console.error("Error fetching popular series:", error));
 
-function getGenresAndDisplay(media, listSection) {
-  const genreUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=en-US`; // Cambiar a la URL de géneros de películas
+function getGenresAndDisplay(series) {
+  const genreUrl = `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=en-US`;
 
   fetch(genreUrl)
     .then((response) => response.json())
@@ -36,19 +22,21 @@ function getGenresAndDisplay(media, listSection) {
         genreMap[genre.id] = genre.name;
       });
 
-      media.forEach((item) => {
-        const genreNames = item.genre_ids.slice(0, 2).map((id) => genreMap[id]);
+      series.forEach((serie) => {
+        const genreNames = serie.genre_ids
+          ? serie.genre_ids.slice(0, 2).map((id) => genreMap[id])
+          : [];
 
-        item.genres = genreNames;
+        serie.genres = genreNames;
       });
 
-      displayMedia(media, listSection);
+      displaySeries(series);
     })
     .catch((error) => console.error("Error fetching genres:", error));
 }
 
-function displayMedia(media, listSection) {
-  media.forEach((item) => {
+function displaySeries(series) {
+  series.forEach((serie) => {
     const article = document.createElement("article");
     article.classList.add("card");
 
@@ -56,8 +44,8 @@ function displayMedia(media, listSection) {
     link.href = "#";
 
     const img = document.createElement("img");
-    img.src = `https://image.tmdb.org/t/p/w200/${item.poster_path}`;
-    img.alt = `Poster of ${item.title || item.name}`;
+    img.src = `https://image.tmdb.org/t/p/w200/${serie.poster_path}`;
+    img.alt = `Poster of ${serie.name}`;
     img.width = 200;
 
     const info = document.createElement("div");
@@ -65,7 +53,7 @@ function displayMedia(media, listSection) {
 
     const title = document.createElement("span");
     title.id = "title";
-    title.textContent = item.title || item.name;
+    title.textContent = serie.name;
 
     const ratingDiv = document.createElement("div");
     ratingDiv.classList.add("rating");
@@ -76,7 +64,7 @@ function displayMedia(media, listSection) {
 
     const ratingNumber = document.createElement("span");
     ratingNumber.id = "number";
-    ratingNumber.textContent = item.vote_average;
+    ratingNumber.textContent = serie.vote_average;
 
     ratingDiv.appendChild(starIcon);
     ratingDiv.appendChild(ratingNumber);
@@ -87,8 +75,8 @@ function displayMedia(media, listSection) {
     const categoriesDiv = document.createElement("div");
     categoriesDiv.classList.add("categories");
 
-    item.genres.forEach((genreName) => {
-      if (genreName != undefined) {
+    serie.genres.forEach((genreName) => {
+      if (genreName) {
         const genreSpan = document.createElement("span");
         genreSpan.textContent = genreName;
         categoriesDiv.appendChild(genreSpan);
@@ -100,8 +88,8 @@ function displayMedia(media, listSection) {
     article.appendChild(link);
     article.appendChild(categoriesDiv);
 
-    listSection.appendChild(article);
+    seriesListSection.appendChild(article);
 
-    console.log(item); // Mostrar información en la consola
+    console.log(serie); // Mostrar información en la consola
   });
 }
